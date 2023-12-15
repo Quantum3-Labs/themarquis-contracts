14c96bdf5c0893bb2358e04ab046f17f57e8dede
CarlosR <carlos.ramos@grupoimportia.com>
2023-12-15 20:29:58 +0800

    update version - carlos

--- l2/GAMEVAULT-Dojo/scripts/deploy.sh
+++ l2/GAMEVAULT-Dojo/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sozo migrate --rpc-url $DOJO_GOERLI_RPC_URL
+sozo migrate --rpc-url https://starknet-goerli.infura.io/v3/c45bd0ce3e584ba4a5e6a5928c9c0b0f
 
 WORLD_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.world.address')
 
