f8761c8319d8d85a2d86048c3627af842e054280
CarlosR <carlos.ramos@grupoimportia.com>
2024-02-13 21:45:37 +0800

    add bot to main branch

--- bot/index.js
+++ bot/index.js
@@ -39,33 +39,33 @@ mainSetup()
   })
   .catch(console.error);
 
-// async function initNewGame() {
-//   printUSDmBalance(theMarquisActionsAddress).then();
-//   // open bets
-//   openBets().then();
+async function initNewGame() {
+  printUSDmBalance(theMarquisActionsAddress).then();
+  // open bets
+  openBets().then();
 
-//   // perform subsequent bets
-//   let leftTime = gameDuration;
-//   while (leftTime > 0) {
-//     const randomNumber = Math.floor(Math.random() * (leftTime / 20 - 2)) + 1500;
-//     placeRandomBet().then();
-//     if (randomNumber < leftTime) await sleep(randomNumber);
-//     leftTime -= randomNumber;
-//   }
+  // perform subsequent bets
+  let leftTime = gameDuration;
+  while (leftTime > 0) {
+    const randomNumber = Math.floor(Math.random() * (leftTime / 20 - 2)) + 1500;
+    placeRandomBet().then();
+    if (randomNumber < leftTime) await sleep(randomNumber);
+    leftTime -= randomNumber;
+  }
 
-//   closeBets().then();
-//   // wait for game to end
-//   await sleep(-leftTime);
-//   // spin the wheel
-//   await sleep(waitForSpinTime);
-//   spinWheel().then();
-//   // wait for spinning
-//   await sleep(spinningDuration);
+  closeBets().then();
+  // wait for game to end
+  await sleep(-leftTime);
+  // spin the wheel
+  await sleep(waitForSpinTime);
+  spinWheel().then();
+  // wait for spinning
+  await sleep(spinningDuration);
 
-//   // set winner
-//   setWinner().then(() => {
-//     printUSDmBalance(theMarquisActionsAddress).then();
-//   });
-// }
+  // set winner
+  setWinner().then(() => {
+    printUSDmBalance(theMarquisActionsAddress).then();
+  });
+}
 
-// cron.schedule("*/2 * * * *", initNewGame);
+cron.schedule("*/2 * * * *", initNewGame);
